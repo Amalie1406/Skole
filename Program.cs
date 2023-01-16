54a5637db070035aa86a3750c5824f5e6e0a13c7
Amalie1406 <103024612+Amalie1406@users.noreply.github.com>
2023-01-16 13:16:16 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace H1GPDag1_oveOpgave
+{
+    class MyConverter
+    {
+        public static void Main(string[] args)
+        {
+
+            double fahrenheit;
+
+            double celsius = 36;
+            Console.WriteLine("Konverter celsius til fahrenheit");
+            Console.WriteLine("Indtast din temperatur:");
+            celsius = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Celsius: " + celsius);
+
+            fahrenheit = (celsius * 9) / 5 + 32;
+            //Console.WriteLine("Fahrenheit: " + fahrenheit);
+            Console.WriteLine($"Fahrenheit: " + fahrenheit);
+
+            Console.Read();
+
+        }
+    }
+}
+
+
